d42f2fef2befa9f396f07b9ef46327a8a1960862
Jason Dinsmore <jason@xhatch.com>
2013-07-03 11:21:15 -0700

    Tweak for sending order

--- lib/jondo/client/orders.rb
+++ lib/jondo/client/orders.rb
@@ -6,11 +6,11 @@ module Jondo
       def order(*args)
         response = post "cofApi.php", args[0], true, true
         parsed = MultiXml.parse(response.body)
-        # 
-        # parsed = MultiXml.parse(response.body)
-        # root = parsed["root"]
-        # reply = root["orderReply"] unless root.nil?
-        # reply
+
+        parsed = MultiXml.parse(response.body)
+        root = parsed["root"]
+        reply = root["orderReply"] unless root.nil?
+        reply
       end
 
     end
